aa37ce2664048dcd3dc45ddd837d235dc9799c13
Tork <guerreromjosefa@gmail.com>
2022-08-29 22:12:18 +0200

    primer intento de que funcione verdadero

--- src/paginas/verdadero/verdadero.jsx
+++ src/paginas/verdadero/verdadero.jsx
@@ -2,20 +2,83 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import BotonInicio from "../../componentes/boton-inicio";
 import './verdadero.css'
+import { useState } from 'react';
 
 
-const opcionesResultados = [1, 2, 3, 4]
+
+
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  const generarOperadores = [getRandomIntInclusive(1, 10), getRandomIntInclusive(1, 10)]  
+  
+  const operacion = `${generarOperadores[0]} * ${generarOperadores[1]}`
+ 
+
+  const resultado = generarOperadores[0] * generarOperadores[1]
+
+  console.log(resultado)
+
+
+  const opcionesResultados = [
+    resultado,
+    getRandomIntInclusive(1, 10),
+    getRandomIntInclusive(1, 10),
+    getRandomIntInclusive(1, 10)
+    ]
+
+  const shuffle = (arr) => {
+    const length = arr.length
+  
+    for (let i = 0; i < length; i++) {
+      const rand_index = Math.floor(Math.random() * length)
+  
+      const rand = arr[rand_index]
+  
+      arr[rand_index] = arr[i]
+      arr[i] = rand
+    }
+  }
+
+  shuffle(opcionesResultados)
+
+
+
 
 function Verdadero() {
+
+    const [opcionSeleccionado, setOpcionSeleccionado] = useState();
+
     return (
         <div className="contenedor-principal-verdadero">
             <h1 className="titulo"> ¿CUÁL ES EL CORRECTO? </h1>
             <div className="contenedor-operacion">
-                <p>4x5</p>
+                <p>{operacion}</p>
             </div>
-            <div className="contenedor-opciones-resultado">
-                {opcionesResultados.map((opcion) => <button className="boton">{opcion}</button>)}
-            </div>
+            <button 
+               // onChange={() =>setOpcionSeleccionado(opcion)}
+               
+                /*onClick={() => {
+                    if (opcionSeleccionado === resultado){
+                        alert('muy bien!')
+                    }else{
+                        alert('sigue intentandolo!')
+                    } }
+                    
+                }*/
+                className="contenedor-opciones-resultado">
+                {opcionesResultados.map((opcion) => 
+                 <button className="boton">{opcion}</button>)
+                
+                }
+                
+                
+            </button>
+            
             <Link to="/">
                 <BotonInicio 
                     texto='Volver al inicio' />
